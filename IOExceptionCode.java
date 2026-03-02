ccea2d0d89ee999877cba999ceff54be851fbd66
UtkarshTrivedi06 <utkarshtrivedi2006@gmail.com>
2026-03-02 10:40:33 +0530

    IOException

---
+++ IOExceptionCode.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+public class IOExceptionCode {
+    IOChild c= new IOChild();
+    IOParent p = new IOParent();
+    c.readFile();
+    p.readFile();
+}
+class IOParent{
+    void readFile() throws IOException{
+        System.out.println("Reading File in Parent");
+    }
+}
+class IOChild extends IOParent{
+    @Override
+    void readFile() throws IOException{
+        System.out.println("Reading File in Child");
+    }
+}
